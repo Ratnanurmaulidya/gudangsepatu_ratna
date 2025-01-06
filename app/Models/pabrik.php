b1c60ce563667ed25774a6aa6a5afc53f698575e
bagusdream <bagus@smkn9malang.sch.id>
2025-01-06 10:21:55 +0700

    first commit

---
+++ app/Models/pabrik.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class pabrik extends Model
+{
+    use HasFactory;
+}
